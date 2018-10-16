bacb60e09427173f1b60c3aa5d5a93bbad30d809
Matías Muñoz Espinoza <writkas2@gmail.com>
2018-10-16 19:48:31 -0300

    Movimiento del personaje y añadiendo .gitignore para ignorar archivos que no son utiles en el proyecto

--- scripts/jugador.gd
+++ scripts/jugador.gd
@@ -8,6 +8,11 @@ var dir ={
 	arriba = Vector2(0,-16),
 	abajo = Vector2(0,16),
 }
+
+# Destino del movimiento
+var dest = Vector2()
+var can_move = true
+
 onready var tilemap = get_parent() #Debe ser hijo de un TileMap
 onready var enemigos = get_tree().get_nodes_in_group("enemigo")
 
@@ -15,6 +20,9 @@ func _physics_process(delta):
 	p = global_position
 
 func _input(event):
+	if not can_move:
+		return
+	
 	var arriba = Input.is_action_just_pressed("arriba")
 	var abajo = Input.is_action_just_pressed("abajo")
 	var derecha = Input.is_action_just_pressed("derecha")
@@ -24,19 +32,30 @@ func _input(event):
 	if arriba:
 		if tilemap.get_cellv(tilemap.world_to_map(p + dir.arriba)) == 0:
 			if !hayEnemigo(dir.arriba):
-				global_position += dir.arriba
+				dest += dir.arriba
 	if abajo:
 		if tilemap.get_cellv(tilemap.world_to_map(p + dir.abajo)) == 0:
 			if !hayEnemigo(dir.abajo):
-				global_position += dir.abajo
+				dest += dir.abajo
 	if derecha:
 		if tilemap.get_cellv(tilemap.world_to_map(p + dir.derecha)) == 0:
 			if !hayEnemigo(dir.derecha):
-				global_position += dir.derecha
+				dest += dir.derecha
 	if izquierda:
 		if tilemap.get_cellv(tilemap.world_to_map(p + dir.izquierda)) == 0:
 			if !hayEnemigo(dir.izquierda):
-				global_position += dir.izquierda
+				dest += dir.izquierda
+	
+	$Movement.interpolate_property(
+		self,
+		"position",
+		global_position,
+		dest,
+		0.4,
+		Tween.TRANS_ELASTIC,
+		Tween.EASE_IN_OUT
+	)
+	$Movement.start()
 	
 	if esperar:
 		mover()
@@ -75,3 +94,10 @@ func _on_areaAtaque_body_exited(body):
 	if body.is_in_group("enemigo"):
 		if enemigosArea.has(body):
 			enemigosArea.erase(body)
+
+
+func _on_Movement_tween_started(object, key):
+	can_move = false
+
+func _on_Movement_tween_completed(object, key):
+	can_move = true
